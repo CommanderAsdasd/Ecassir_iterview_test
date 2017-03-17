8ebf2e051ea791af3d1954f26d947e115695bea1
Precautionary <e1ectricwiz4rd@gmail.com>
2017-03-17 13:47:30 +0400

    new file:   array_parse.css 	new file:   index.html 	modified:   main.js 	new file:   page_dummy.html 	deleted:    main.html 	deleted:    page.html 	deleted:    script.html 	deleted:    script_with_jquery_include.html 	deleted:    timer.html

--- main.js
+++ main.js
@@ -1,17 +1,66 @@
-$(document).ready(function(){	
-})
+//test data
+var bar = function() {}; 
+foo = {test : "5", lol : 55};
+var a = [1, 2, 3, null, undefined, 'a', 'b', 'c', '', true, false, 31, 42, 53, , function () { }, {}, [], 15, 55, 4];
 
-function parsing_array(array) {
-	var plug_jQuery = function() {
-		var jQuery_script = document.createElement("script");
-	    jQuery_script.type = "text/javascript";
-	    jQuery_script.src = "//code.jquery.com/jquery-3.1.1.min.js";
-	    var head_var = document.getElementsByTagName('head')[0] || new_doc.document.head;	
-	    head_var.appendChild(jQuery_script);
+function divisibility_check(number) {
+	if (number % 3 === 0 && number % 5 === 0) {
+		return "fizzbuzz"; 
+	} 
+	else if (number % 3 === 0) {
+		return "fizz"; 
 	}
+	else if (number % 5 === 0) {
+		return "buzz"; 
+	}
+	else {
+		return number; 
+	} 
+};
 
-	var new_doc = window.open("");
-	new_doc.document.write("<html><head><title>testpage</title></head><body></body></html>");
-	
-	parsing_array.plug_jQuery(head_var);
-}
+function string_check(string) {
+	if (string) {
+		return string;
+	} 
+	else {
+		return "string is empty"
+	}
+};
+function place_to_page(processed_array) {
+	$("body").append("<div class=\"container\"></div>");
+	for (i in processed_array) {		
+	$(".container").append("<div class=\"inner_block\">" + processed_array[i] + "</div>");
+	}
+};
+
+	function parsing_array(input_array) {
+	var curr_element;
+	var appended_text = [];
+	for (var i = 0; i < input_array.length; i++) {
+		curr_element = input_array[i];
+		if (typeof(curr_element) === "number") {
+			appended_text[i] = divisibility_check(curr_element);
+		}
+		else if (typeof(curr_element) === "string") {
+			appended_text[i] = string_check(curr_element);
+		}
+		else if (typeof(curr_element) === "function") {
+			appended_text[i] = "element is function";
+		}
+		else if (typeof(curr_element) === "object" && curr_element !== null && !Array.isArray(curr_element)) {
+			appended_text[i] = "element is Object";
+		}
+		else if (Array.isArray(curr_element)) {
+			appended_text[i] = "element is Array";
+		}
+		else if (typeof(curr_element) === "undefined")
+			appended_text[i] = "element is Undefined";
+		else if (curr_element === null) {
+			appended_text[i] = "element is null";
+		}
+	}
+	place_to_page(appended_text);
+};
+
+$(document).ready(function(){
+});
